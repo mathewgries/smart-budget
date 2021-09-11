a00e5fc4e0887bb48e8c4e5c813e60d1a1e2458d
mathewgries <mathewgries@gmail.com>
2021-09-11 12:20:37 -0400

    Added Stacks

---
+++ src/create.js
@@ -0,0 +1,34 @@
+import * as uuid from "uuid";
+import AWS from "aws-sdk";
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+export async function main(event) {
+  // Request body is passed in as a JSON encoded string in 'event.body'
+  const data = JSON.parse(event.body);
+
+  const params = {
+    TableName: process.env.TABLE_NAME,
+    Item: {
+      // The attributes of the item to be created
+      PK: "123", // The id of the author
+      SK: `ACCT#${uuid.v1()}`, // A unique uuid
+      accountName: data.accountName, // Parsed from request body
+      createdAt: Date.now(), // Current Unix timestamp
+    },
+  };
+
+  try {
+    await dynamoDb.put(params).promise();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(params.Item),
+    };
+  } catch (e) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: e.message }),
+    };
+  }
+}
