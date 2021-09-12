7d7dc2ea78649b2cd484d90a90637203106b6d07
mathewgries <mathewgries@gmail.com>
2021-09-12 11:35:43 -0400

    Added Auth Stack

--- src/create.js
+++ src/create.js
@@ -1,34 +1,23 @@
 import * as uuid from "uuid";
-import AWS from "aws-sdk";
+import handler from "./util/handler";
+import dynamoDb from "./util/dynamodb";
 
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
-export async function main(event) {
-  // Request body is passed in as a JSON encoded string in 'event.body'
+export const main = handler(async (event) => {
   const data = JSON.parse(event.body);
 
   const params = {
     TableName: process.env.TABLE_NAME,
     Item: {
       // The attributes of the item to be created
-      PK: "123", // The id of the author
-      SK: `ACCT#${uuid.v1()}`, // A unique uuid
+      PK: "USER#123", // The id of the author
+      SK: `USER#123#ACCT#${uuid.v1()}`, // A unique uuid
       accountName: data.accountName, // Parsed from request body
       createdAt: Date.now(), // Current Unix timestamp
     },
   };
 
-  try {
-    await dynamoDb.put(params).promise();
+  await dynamoDb.put(params);
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify(params.Item),
-    };
-  } catch (e) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: e.message }),
-    };
-  }
-}
+  return params.Item;
+
+});
