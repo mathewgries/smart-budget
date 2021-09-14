89ac38ecd22dff6e8541a5f28ead1b598467c945
mathewgries <mathewgries@gmail.com>
2021-09-13 20:40:20 -0400

    Refactor transaction update and create

--- src/functions/transactions/create.js
+++ src/functions/transactions/create.js
@@ -8,22 +8,46 @@ export const main = handler(async (event) => {
   const accountId = data.accountId;
 
   const params = {
-    TableName: process.env.TABLE_NAME,
-    Item: {
-      PK: `ACCT#${accountId}`,
-      SK: `TRANS#${uuid.v1()}`,
-      GSI_user_trans_PK: `USER#${userId}`,
-      transactionAmount: data.transactionAmount,
-      transactionDate: data.transactionDate,
-      transactionType: data.transactionType,
-      category: data.category,
-      subCategory: data.subCategory,
-      createDate: Date.now(), // Current Unix timestamp
-      modifyDate: Date.now(), // Current Unix timestamp
-    },
+    TransactItems: [
+      {
+        Put: {
+          TableName: process.env.TABLE_NAME,
+          Item: {
+            PK: `ACCT#${accountId}`,
+            SK: `TRANS#${uuid.v1()}`,
+            GSI_user_trans_PK: `USER#${userId}`,
+            transactionAmount: data.transactionAmount,
+            transactionDate: data.transactionDate,
+            transactionType: data.transactionType,
+            category: data.category,
+            subCategory: data.subCategory,
+            createDate: Date.now(), // Current Unix timestamp
+            modifyDate: Date.now(), // Current Unix timestamp
+          },
+        },
+      },
+      {
+        Update: {
+          TableName: process.env.TABLE_NAME,
+          Key: {
+            PK: `USER#${userId}`,
+            SK: `ACCT#${accountId}`,
+          },
+          UpdateExpression:
+            "SET accountBalance = :accountBalance, modifyDate = :modifyDate",
+          ExpressionAttributeValues: {
+            ":accountBalance":
+              data.transactionType === "W"
+                ? data.accountBalance - data.transactionAmount
+                : data.accountBalance + data.transactionAmount,
+            ":modifyDate": Date.now(),
+          },
+        },
+      },
+    ],
   };
 
-  await dynamoDb.put(params);
+  await dynamoDb.transactWrite(params);
 
-  return params.Item;
+  return params.TransactItems.Item;
 });
--- src/functions/transactions/update.js
+++ src/functions/transactions/update.js
@@ -1,36 +1,62 @@
 import handler from "../../util/handler";
 import dynamodb from "../../util/dynamodb";
+import updateTransactionHelper from "../../helpers/updateTransactionHelper";
 
 export const main = handler(async (event) => {
   const data = JSON.parse(event.body);
+  const oldData = data.oldData;
+  const newData = data.newData;
+  const userId = event.requestContext.authorizer.iam.cognitoIdentity.identityId;
   const accountId = data.accountId;
   const transactionId = event.pathParameters.id;
 
-  const paramsTrans = {
-    TableName: process.env.TABLE_NAME,
-    Key: {
-      PK: `ACCT#${accountId}`,
-      SK: `TRANS#${transactionId}`,
-    },
-    UpdateExpression: `SET 
-    transactionAmount = :transactionAmount, 
-    transactionDate = :transactionDate, 
-    transactionType = :transactionType, 
-    category = :category, 
-    subCategory = :subCategory,
-    modifyDate = :modifyDate`,
-    ExpressionAttributeValues: {
-      ":transactionAmount": data.transactionAmount || null,
-      ":transactionDate": data.transactionDate || null,
-      ":transactionType": data.transactionType || null,
-      ":category": data.category || null,
-      ":subCategory": data.subCategory || null,
-      ":modifyDate": Date.now(),
-    },
+  const params = {
+    TransactItems: [
+      {
+        Update: {
+          TableName: process.env.TABLE_NAME,
+          Key: {
+            PK: `ACCT#${accountId}`,
+            SK: `TRANS#${transactionId}`,
+          },
+          UpdateExpression: `SET 
+          transactionAmount = :transactionAmount, 
+          transactionDate = :transactionDate, 
+          transactionType = :transactionType, 
+          category = :category, 
+          subCategory = :subCategory,
+          modifyDate = :modifyDate`,
+          ExpressionAttributeValues: {
+            ":transactionAmount": newData.transactionAmount || null,
+            ":transactionDate": newData.transactionDate || null,
+            ":transactionType": newData.transactionType || null,
+            ":category": newData.category || null,
+            ":subCategory": newData.subCategory || null,
+            ":modifyDate": Date.now(),
+          },
+        },
+      },
+      {
+        Update: {
+          TableName: process.env.TABLE_NAME,
+          Key: {
+            PK: `USER#${userId}`,
+            SK: `ACCT#${accountId}`,
+          },
+          UpdateExpression:
+            "SET accountBalance = :accountBalance, modifyDate = :modifyDate",
+          ExpressionAttributeValues: {
+            ":accountBalance": updateTransactionHelper(oldData,newData,data.accountBalance),
+            ":modifyDate": Date.now(),
+          },
+        },
+      },
+    ],
+
     ReturnValues: "ALL_NEW",
   };
 
-  await dynamodb.update(paramsTrans);
+  await dynamodb.transactWrite(params);
 
   return { status: true };
 });
