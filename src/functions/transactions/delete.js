b6228f88b492c3177bce1c5374ddf6827220a03d
mathewgries <mathewgries@gmail.com>
2021-09-14 15:12:13 -0400

    Added account update to trans delete

--- src/functions/transactions/delete.js
+++ src/functions/transactions/delete.js
@@ -3,15 +3,40 @@ import dynamoDb from "../../util/dynamodb";
 
 export const main = handler(async (event) => {
   const data = JSON.parse(event.body);
+  const userId = event.requestContext.authorizer.iam.cognitoIdentity.identityId;
   const accountId = data.accountId;
   const transactionId = event.pathParameters.id;
 
   const params = {
-    TableName: process.env.TABLE_NAME,
-    Key: {
-      PK: `ACCT#${accountId}`,
-      SK: `TRANS#${transactionId}`,
-    },
+    TransactWrite: [
+      {
+        Delete: {
+          TableName: process.env.TABLE_NAME,
+          Key: {
+            PK: `ACCT#${accountId}`,
+            SK: `TRANS#${transactionId}`,
+          },
+        },
+      },
+      {
+        Update: {
+          TableName: process.env.TABLE_NAME,
+          Key: {
+            PK: `USER#${userId}`,
+            SK: `ACCT#${accountId}`,
+          },
+          UpdateExpression:
+            "SET accountBalance = :accountBalance, modifyDate = :modifyDate",
+          ExpressionAttributeValues: {
+            ":accountBalance":
+              data.transactionType === "W"
+                ? data.accountBalance + data.transactionAmount
+                : data.accountBalance - data.transactionAmount,
+            ":modifyDate": Date.now(),
+          },
+        },
+      },
+    ],
   };
 
   await dynamoDb.delete(params);
