7ee5e8969ce4af46c5708e0bc0e26bcb4634ecf2
mathewgries <mathewgries@gmail.com>
2022-04-19 12:50:24 -0400

    Setting up new branch 041922

--- frontend/src/containers/investing/journal/InvestingJournal.js
+++ frontend/src/containers/investing/journal/InvestingJournal.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { dollarsToCents } from "../../../helpers/currencyHandler";
 import { selectInvestingAccountById } from "../../../redux/investing/investingAccountsSlice";
 import {
   selectSharesOrdersByAccounGSI,
